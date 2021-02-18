d2ffc3cdbe4a3c5736cae3575f56ba821c04e9a3
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-02-18 18:37:42 +0400

    removed static serve from root path

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use('/buildings/:workspaceId', express.static(path.join(__dirname, '../', 'c
 app.get('/api/availability', forwardingController(process.env.CONTACT_API, 3001));
 app.get('/workspace-api/workspace/:id', forwardingController(process.env.WORKSPACE_API, 4000));
 app.get('/api/photos/:id', forwardingController(process.env.PHOTOS_API, 6001));
+app.get('/api/photos/workspace/:id', forwardingController(process.env.PHOTOS_API, 6001));
 app.get('/amenities-api/amenity/:id', forwardingController(process.env.AMENITIES_API, 4001));
 app.get('/api/workspace-description/:id', forwardingController(process.env.DESCRIPTION_API, 6060));
 app.get('/api/nearbyworkspaces/buildings/:id', forwardingController(process.env.NEARBY_API, 5001));
@@ -19,7 +20,6 @@ app.use((err, req, res, next) => {
   res.json();
 });
 
-app.use('/', express.static(path.join(__dirname, '../', 'client', 'dist')));
 const PORT = process.env.PORT || 5000;
 exports.server = app.listen(PORT, console.log('app running'));
 exports.app = app;
